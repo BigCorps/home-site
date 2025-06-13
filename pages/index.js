1ff72b9594ae56f1612df5300930b37ae4541de7
BigCorps <interpag.bigcorps@gmail.com>
2025-06-13 10:11:35 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ export default function Home() {
   const redirectUrl = "https://8tihnvjgf4rc1q5pbcup.share.dreamflow.app/"; // Troque pelo link desejado!
 
   useEffect(() => {
-    // Mensagem antes de redirecionar
-    alert("Bem-vindo ao App DashBoard BigCorps! Você será redirecionado.");
-    // Redireciona após 2 segundos
+    alert("Bem-vindo ao BigCorps App! Você será redirecionado.");
     setTimeout(() => {
       window.location.href = redirectUrl;
     }, 2000);
@@ -15,13 +13,6 @@ export default function Home() {
 
   return (
     <>
-      <head>
-        <title>BigCorps App</title>
-        <meta name="theme-color" content="#317EFB"/>
-        <meta name="description" content="Acesse rapidamente pelo app do BigCorps!"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <link rel="icon" href="/icons/icon-192.png"/>
-      </head>
       <main style={{textAlign: 'center', marginTop: '40vh'}}>
         <h1>BigCorps App</h1>
         <p>Você será redirecionado em instantes...</p>
